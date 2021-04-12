e5a245c51e85fba4a3a9a7197a798ff07b9de74e
Huong Nguyen <79285136+htnguyen23@users.noreply.github.com>
2021-04-12 14:41:47 -0500

    Update BackendInterface.java
    Integration changes.

--- BackendInterface.java
+++ BackendInterface.java
@@ -1,14 +1,27 @@
+// --== CS400 File Header Information ==--
+// Name: Casey Waddell
+// Email: ctwaddell@wisc.edu
+// Team: KB
+// TA: Keren
+// Lecturer: Gary
+// Notes to Grader: 
+
 import java.util.LinkedList;
 
-public interface BackendInterface 
-{
-  public CS400Graph<Park>.Path returnPathTo(Park destination);
-  public CS400Graph<Park>.Path fromTo(Park departure, Park destination);
-  public boolean doesParkExist(String searchQuery);
-  public LinkedList<Park> findParks(String searchQuery);
-  public Park randomPark();
-  //I will only implement the bottom ones if I have time or if we want them bad enough
-  //essentially they let you add a stop in between the departure and destination
-  //public Path fromToTo(Vertex departure, Vertex stop, Vertex destination);
-  //public Path fromToToTo(Vertex departure, Vertex stop1, Vertex stop2, Vertex destination);
+public interface BackendInterface {
+	public CS400Graph<Park>.Path returnPathTo(Park destination);
+
+	public CS400Graph<Park>.Path fromTo(Park departure, Park destination);
+
+	public boolean doesParkExist(String searchQuery);
+
+	public Park[] findParks();
+
+	public Park randomPark();
+	// I will only implement the bottom ones if I have time or if we want them bad
+	// enough
+	// essentially they let you add a stop in between the departure and destination
+	// public Path fromToTo(Vertex departure, Vertex stop, Vertex destination);
+	// public Path fromToToTo(Vertex departure, Vertex stop1, Vertex stop2, Vertex
+	// destination);
 }
