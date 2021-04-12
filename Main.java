5dd7bd61fe537dbb45b7eb1ed8c51ee5b2e566c4
Huong Nguyen <79285136+htnguyen23@users.noreply.github.com>
2021-04-12 14:40:52 -0500

    Update Main.java
    Integration changes.

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
+import java.util.zip.DataFormatException;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
-			Backend graph = new Backend("national_parks.csv", "National Park Descriptions and States.csv");
+			//IM: changed order of filenames to match Backend's constructor
+			Backend graph = new Backend("national_park_descriptions_and_states.csv", "national_parks.csv");
 			Frontend run = new Frontend(graph);
 			run.baseMode();
 		}
 		catch (IOException e) {
 			System.out.println("Could not import file or run program.");
+		} catch (DataFormatException e) {
+			System.out.println("DataFormatException: Could not import file or run program.");
 		}
 	}
 }
